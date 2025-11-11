9495f4ca2679e3d1db12ff08b6ee5a31b94a8f1f
Billy Harrold <Billy2.Harrold@live.uwe.ac.uk>
2025-11-11 12:02:52 +0000

    Raycast hits on Tagged objects

---
+++ Assets/Scripts/OnTouch.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class OnTouch : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            //Debug.Log("CLICKED");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Debug.Log("hit");
+                Debug.Log(hit.transform.name + " : " + hit.transform.tag);
+            }
+        }
+
+    }
+}
