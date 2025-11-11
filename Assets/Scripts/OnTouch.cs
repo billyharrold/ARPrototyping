6214f6a9bb524c63f1e73c5fd4331370900af740
Billy Harrold <Billy2.Harrold@live.uwe.ac.uk>
2025-11-11 12:26:03 +0000

    Spawns at child of Enzyme
    Rotations and positions are wonky but can be fixed

--- Assets/Scripts/OnTouch.cs
+++ Assets/Scripts/OnTouch.cs
@@ -2,16 +2,31 @@ using UnityEngine;
 
 public class OnTouch : MonoBehaviour
 {
+
+    public GameObject enzymePopUp;
+    public Transform child;
+    //public GameObject enzyme;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (child == null)
+        {
+            Transform found = transform.Find("Info Point");
+            if (found != null)
+                child = found;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+       
+        OnClicked();
+    }
+
+    void OnClicked()
+    {
+        if (Input.GetMouseButtonDown(0))
         {
             //Debug.Log("CLICKED");
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -20,8 +35,21 @@ public class OnTouch : MonoBehaviour
             {
                 Debug.Log("hit");
                 Debug.Log(hit.transform.name + " : " + hit.transform.tag);
+
+                if (hit.transform.tag == "Enzyme")
+                {
+                    Vector3 pos = hit.point;
+                    
+                    //pos.z += 0.25f;
+                    //pos.y += 0.25f;
+                    Instantiate(enzymePopUp, child.transform.position, transform.rotation);
+                }
             }
         }
-
     }
+
+    //void getChildPos(GameObject info_spawn)
+    //{
+    //    info_spawn = enzyme.GetComponentInChildren<GameObject>();
+    //}
 }
