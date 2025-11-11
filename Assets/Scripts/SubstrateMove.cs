df0c961919a3ee83e8466ffb17cd997858b94729
Billy Harrold <Billy2.Harrold@live.uwe.ac.uk>
2025-11-11 19:11:52 +0000

    Reactive Sequence
    Added in substrate reacted models. They animate after the substrate moves in. Just need to reset and add materials

--- Assets/Scripts/SubstrateMove.cs
+++ Assets/Scripts/SubstrateMove.cs
@@ -11,6 +11,16 @@ public class SubstrateMove : MonoBehaviour
     
     private Transform activeSite;
     private Transform spawnPoint;
+
+
+    public GameObject LSubstrate;
+    public GameObject RSubstrate;
+
+    private Transform p1;
+    private Transform p2;
+
+    private Transform b1;
+    private Transform b2;
     
     private bool merging = false;
 
@@ -18,6 +28,8 @@ public class SubstrateMove : MonoBehaviour
     public float rotationSpeed = 2f;
 
     private Renderer rend;
+
+    //private bool arrived = false;
 
 
     void Start()
@@ -29,6 +41,13 @@ public class SubstrateMove : MonoBehaviour
 
         spawnPoint = enzyme.transform.Find("Spawn Point");
         activeSite = enzyme.transform.Find("Active Site");
+
+        p1 = enzyme.transform.Find("P1");
+        p2 = enzyme.transform.Find("P2");
+
+        b1 = enzyme.transform.Find("B1");
+        b2 = enzyme.transform.Find("B2");
+
         if (activeSite == null )
         {
             Debug.LogWarning("No active site found");
@@ -38,7 +57,7 @@ public class SubstrateMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float distance = Vector3.Distance(activeSite.transform.position, enzyme.transform.position);
+        float distance = Vector3.Distance(activeSite.transform.position, transform.position);
 
         rend.enabled = merging;
 
@@ -58,11 +77,19 @@ public class SubstrateMove : MonoBehaviour
         {
             transform.position = Vector3.Lerp(transform.position, activeSite.position, Time.deltaTime * moveSpeed);
             transform.rotation = Quaternion.Lerp(transform.rotation, activeSite.rotation, Time.deltaTime * rotationSpeed);
+            //arrived = true;
         }
         if (!merging)
         {
             
             transform.position = spawnPoint.position;
+           // ResetReaction();
+            //arrived = false;
+        }
+
+        if (distance < 0.01f)
+        {
+            React();
         }
 
     }
@@ -81,6 +108,16 @@ public class SubstrateMove : MonoBehaviour
         //merging = false;    
     }
 
-   
+    private void React()
+    {
+        LSubstrate.transform.position = Vector3.Lerp(LSubstrate.transform.position, p1.position, Time.deltaTime * moveSpeed);
+        RSubstrate.transform.position = Vector3.Lerp(RSubstrate.transform.position, p2.position, Time.deltaTime * moveSpeed);
+    }
+
+    private void ResetReaction()
+    {
+        LSubstrate.transform.position = b1.position;
+        RSubstrate.transform.position= b2.position;
+    }
 
 }
